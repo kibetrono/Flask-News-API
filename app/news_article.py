93fc56ed44cf73f10c9b62940dcde1d388cb5f3f
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-30 17:57:16 +0300

    add articles class

---
+++ app/news_article.py
@@ -0,0 +1,11 @@
+class NewsArticles:
+    """class that defines news articles"""
+
+    def __init__(self,author, title, description, url, urlToImage, publishedAt, content):
+        self.author=author
+        self.title=title
+        self.description=description
+        self.url = url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
+        self.content=content
