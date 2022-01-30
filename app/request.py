cf40309cb57f212aa35e07b451dd8d108f3daf4a
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-30 22:03:21 +0300

    add method that fetches and process news articles

--- app/request.py
+++ app/request.py
@@ -51,3 +51,53 @@ def process_results_sources(news_list):
         news_results.append(news_object)
 
     return news_results
+
+
+# ----------------------------------------Articles----------------------------------
+
+def get_articles_news(source):
+    get_news_articles_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(source, api_key)
+
+    news_art = requests.get(get_news_articles_url).json()
+
+    get_news_articles_url = None
+
+    if news_art['articles']:
+        news_art_url = news_art['articles']
+        get_news_articles_url = process_results_articles(news_art_url)
+
+    return get_news_articles_url
+
+
+def articles_source(source, pageSize):
+    get_articles_by_source = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}&pageSize={}'.format(source,
+                                                                                                            api_key,
+                                                                                                            pageSize)
+    news_articles_source_response = requests.get(get_articles_by_source).json()
+
+    articles_source_results = None
+
+    if news_articles_source_response['articles']:
+        news_res = news_articles_source_response['articles']
+        articles_source_results = process_results_articles(news_res)
+
+    return articles_source_results
+
+def process_results_articles(news_articles):
+    """Function  that processes the news articles results and transform them to a list of Objects"""
+    news_articles_process_results = []
+
+    for eachnews in news_articles:
+        author = eachnews.get('author')
+        title = eachnews.get('title')
+        description = eachnews.get('description')
+        url = eachnews.get('url')
+        urlToImage = eachnews.get('urlToImage')
+        publishedAt = eachnews.get('publishedAt')
+        content = eachnews.get('content')
+
+        if urlToImage:
+            news_object = NewsArticles(author, title, description, url, urlToImage, publishedAt, content)
+            news_articles_process_results.append(news_object)
+
+    return news_articles_process_results
