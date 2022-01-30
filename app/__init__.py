05a9a2e345e42a481f5181fdc02aec3028127f3f
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-30 18:01:10 +0300

    add the app's factory file

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # app configurations
+    app.config.from_object(config_options[config_name])
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    # Register blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
